4213cb762a565235cc29d9c65c20326ad34ae26d
Harshake <harshake.pandit12@gmail.com>
2026-04-27 18:11:33 +0530

    add: discovery service

--- discovery-service/src/main/java/com/myHealthBox/discovery_service/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/com/myHealthBox/discovery_service/DiscoveryServiceApplication.java
@@ -2,8 +2,9 @@ package com.myHealthBox.discovery_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-
+@EnableEurekaServer
 @SpringBootApplication
 public class DiscoveryServiceApplication {
 
